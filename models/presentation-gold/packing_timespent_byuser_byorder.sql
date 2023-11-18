16757ad9d64f1cae21c6c60217757a8bb477509e
Morgan Cabot <morgan.cabot@gmail.com>
2023-11-18 04:48:31 +0000

    adding basic packing operations to the model

--- models/presentation-gold/packing_timespent_byuser_byorder.sql
+++ models/presentation-gold/packing_timespent_byuser_byorder.sql
@@ -4,6 +4,33 @@ Assumptions:
     - As such, we cannot estimate the pack time for the last order
     - Because an entire order has the same timestamp, we can only calculate the time to pack an entire order, not a given article
     - This model does not take into account the fact that the user needs to take breaks, break expectations should be added
+
+Confirmation of assumptions:
+
+- Users who are packing orders only pack orders, they are not moving in between tasks: 
+
+select distinct
+    user_id,
+    action_code
+from {{ref('dim_transactions')}}
+where user_id in (select user_id from {{ref('dim_transactions')}} where action_code = 'PKOCLOSE')
+order by 1
+
+This allows me to create a CTE isolating only packing transactions, and trust that subsequent tasks in this subsquery happened sequentially
+
+- A user will always start packing an entire order at the same time:
+
+select 
+    order_number,
+    user_id,
+    count(distinct transaction_timestamp)
+from {{ ref("dim_transactions") }}
+where action_code = 'PKOCLOSE'
+group by 1,2
+order by 3 desc 
+
+This allows me to select distinct orders and users, in order to calculate the time spent on each order packed
+
 */
 
 with
@@ -43,16 +70,24 @@ with
             datediff(second, pack_timestamp, next_timestamp) as time_spent_packing
         from distinct_pack_orders
     ),
-    pack_standards as (
-        select a.material as article, a.department, time_standard
-        from {{ ref("dim_articles") }} a
-        left join {{ ref("lookup_packing_standards") }} s on a.department = s.department
+    pack_article_standards as (
+        select article, department, time_standard
+        from {{ ref("lookup_packing_article_standards") }} 
     ),
-    time_spent_expected as (
+    pack_process_standards as (
+        select sum(time_standard) as order_process_time_standard
+        from {{ref('lookup_packing_process_standards')}}
+    ),
+    time_spent_articles_expected as (
         select order_number, sum(time_standard * quantity) as time_expected
         from pack_orders po
-        left join pack_standards ps on po.article = ps.article
+        left join pack_article_standards ps on po.article = ps.article
         group by order_number
+    ),
+    time_spent_expected as (
+        select order_number, time_expected + order_process_time_standard as time_expected
+        from time_spent_articles_expected
+        left join pack_process_standards on 1=1 -- join this value for all order
     )
 select
     actual.pack_date,
